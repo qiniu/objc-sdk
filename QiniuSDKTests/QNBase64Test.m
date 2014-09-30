0d22bf010db018aea307a4071e7f55ef29795a86
longbai <slongbai@gmail.com>
2014-09-30 22:56:37 +0800

    first unit test

---
+++ QiniuSDKTests/QNBase64Test.m
@@ -0,0 +1,27 @@
+//
+//  QNBase64Test.m
+//  QiniuSDK
+//
+//  Created by bailong on 14/9/30.
+//  Copyright (c) 2014年 Qiniu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "QiniuSDK.h"
+
+@interface QNBase64Test : XCTestCase
+
+- (void)testEncode;
+
+@end
+
+@implementation QNBase64Test
+
+- (void)testEncode {
+    // This is an example of a functional test case.
+    NSString *source = @"你好/+=";
+    XCTAssert([[QNBase64 encode:source] isEqual:@"5L2g5aW9Lys9"], @"Pass");
+}
+
+
+@end
