5810c05c216122c6b78fa50e804949d2612bcee6
longbai <slongbai@gmail.com>
2014-10-17 18:06:29 +0800

    add check error

---
+++ QiniuSDK/Common/QNAsyncRun.h
@@ -0,0 +1,13 @@
+//
+//  QNAsyncRun.h
+//  QiniuSDK
+//
+//  Created by bailong on 14/10/17.
+//  Copyright (c) 2014年 Qiniu. All rights reserved.
+//
+
+#ifndef QiniuSDK_QNAsyncRun_h
+#define QiniuSDK_QNAsyncRun_h
+
+
+#endif
---
+++ QiniuSDK/Common/QNAsyncRun.m
@@ -0,0 +1,9 @@
+//
+//  QNAsyncRun.m
+//  QiniuSDK
+//
+//  Created by bailong on 14/10/17.
+//  Copyright (c) 2014年 Qiniu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
