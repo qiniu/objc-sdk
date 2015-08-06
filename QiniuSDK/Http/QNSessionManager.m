d94c380e40de5214abbee3d5f9331b446d20f6d2
Joe Zhou <zhoushuqun@gmail.com>
2015-08-07 04:03:46 +0800

    Fix filename(case-sensitive) of HappyDNS header.

--- QiniuSDK/Http/QNSessionManager.m
+++ QiniuSDK/Http/QNSessionManager.m
@@ -14,7 +14,7 @@
 #import "QNResponseInfo.h"
 #import "QNAsyncRun.h"
 #import "QNDns.h"
-#import "HappyDns.h"
+#import "HappyDNS.h"
 
 #if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000) || (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1090)
 
