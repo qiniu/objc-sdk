9928c6af7e13a0d7a7e9abc7baa5dcd4e5ee9f5e
heshu <1132628199@qq.com>
2015-10-28 17:01:37 +0800

    update final 4

--- QiniuSDK/Common/QNPHAssetFile.m
+++ QiniuSDK/Common/QNPHAssetFile.m
@@ -6,9 +6,8 @@
 //
 //
 
-#import "QNPHAssetFile.h"
-
 #if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000) || (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1090)
+#import "QNPHAssetFile.h"
 #import <Photos/Photos.h>
 
 enum {
