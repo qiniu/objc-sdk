ddec0f45ea97d4b6a63c015c818bc846a8440fdc
Aaron <1132628199@qq.com>
2015-11-30 19:01:00 +0800

    update

--- QiniuSDK/Storage/QNResumeUpload.m
+++ QiniuSDK/Storage/QNResumeUpload.m
@@ -11,7 +11,6 @@
 #import "QNUrlSafeBase64.h"
 #import "QNConfiguration.h"
 #import "QNResponseInfo.h"
-//#import "QNHttpManager.h"
 #import "QNUploadOption+Private.h"
 #import "QNRecorderDelegate.h"
 #import "QNCrc32.h"
