61f6a895363c07abe2354c10173728baaa36dc53
Aaron <1132628199@qq.com>
2016-03-09 16:05:34 +0800

    add Demo

---
+++ QiniuDemo/QiniuDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QiniuDemo
+//
+//  Created by   何舒 on 16/3/2.
+//  Copyright © 2016年 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
