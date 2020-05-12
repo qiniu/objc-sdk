92543e70b86035d4a31dabda74dfee12de07f9d5
XpSun <teamwork_SunXp@163.com>
2020-05-12 11:17:24 +0800

    updateversion 7.3.3

--- QiniuSDKTests/QNTestConfig.h
+++ QiniuSDKTests/QNTestConfig.h
@@ -6,4 +6,4 @@
 //  Copyright (c) 2014年 Qiniu. All rights reserved.
 //
 
-static NSString *const g_token = @"bjtWBQXrcxgo7HWwlC_bgHg81j352_GhgBGZPeOW:xqqLWq-eaaIMfbL0-T1lL8AGl9c=:eyJzY29wZSI6InNodWFuZ2h1bzEiLCJkZWFkbGluZSI6MTU4ODA2Nzg3NX0K";
+static NSString *const g_token = @"bjtWBQXrcxgo7HWwlC_bgHg81j352_GhgBGZPeOW:bx3SEGbnAAk-AwUEzDP866Ydqio=:eyJzY29wZSI6InNodWFuZ2h1bzEiLCJkZWFkbGluZSI6MTU4OTMzOTc0M30K";
