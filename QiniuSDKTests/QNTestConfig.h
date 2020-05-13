bfe89f932f72fdc89efa2fe5e3289d0b40b8c4af
chupei <chupei@qiniu.com>
2020-05-13 15:05:06 +0800

    network chaos test

--- QiniuSDKTests/QNTestConfig.h
+++ QiniuSDKTests/QNTestConfig.h
@@ -6,4 +6,4 @@
 //  Copyright (c) 2014年 Qiniu. All rights reserved.
 //
 
-static NSString *const g_token = @"bjtWBQXrcxgo7HWwlC_bgHg81j352_GhgBGZPeOW:bx3SEGbnAAk-AwUEzDP866Ydqio=:eyJzY29wZSI6InNodWFuZ2h1bzEiLCJkZWFkbGluZSI6MTU4OTMzOTc0M30K";
+static NSString *const g_token = @"fjxKGs5obR6WYH39j3wnHJN-kRQvFBURvsxLFp3D:NtAkaweFs_zfExSNY3mzIEOpeuI=:eyJzY29wZSI6InB1YmxpY2J1Y2tldF96MCIsImlzUHJlZml4YWxTY29wZSI6MCwiZGVhZGxpbmUiOjE1ODkzNTY0ODEsImFjY2Vzc2VzIjpudWxsfQ==";
