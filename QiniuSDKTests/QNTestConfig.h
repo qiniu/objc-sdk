450f3a00b92bef91e8a993097b9fdfe9669b2390
XpSun <teamwork_SunXp@163.com>
2020-08-13 17:58:37 +0800

    update test token

--- QiniuSDKTests/QNTestConfig.h
+++ QiniuSDKTests/QNTestConfig.h
@@ -6,4 +6,4 @@
 //  Copyright (c) 2014年 Qiniu. All rights reserved.
 //
 
-static NSString *const g_token = @"bjtWBQXrcxgo7HWwlC_bgHg81j352_GhgBGZPeOW:NCi3RlX19n-2GF2GStWahoSBJHI=:eyJzY29wZSI6InB1cnN1ZSIsImRlYWRsaW5lIjoxNTk3Mzk2NjYxfQo=";
+static NSString *const g_token = @"jH983zIUFIP1OVumiBVGeAfiLYJvwrF45S-t22eu:DhJHSpDW0NW6luUhQUvGKmBEmT0=:eyJzY29wZSI6InpvbmUwLXNwYWNlIiwiZGVhZGxpbmUiOjE2MDI0OTY2MjUsICJyZXR1cm5Cb2R5Ijoie1wiZm9vXCI6JCh4OmZvbyksIFwiYmFyXCI6JCh4OmJhciksIFwibWltZVR5cGVcIjokKG1pbWVUeXBlKSwgXCJoYXNoXCI6JChldGFnKSwgXCJrZXlcIjokKGtleSksIFwiZm5hbWVcIjokKGZuYW1lKX0ifQ==";
