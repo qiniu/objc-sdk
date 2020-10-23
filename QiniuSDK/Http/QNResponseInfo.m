15332230f304141c3f631345dc9e5bed51bb9641
YangSen-qn <yang_sen_ysys@163.com>
2020-10-23 14:43:24 +0800

    change host retry logic

--- QiniuSDK/Http/QNResponseInfo.m
+++ QiniuSDK/Http/QNResponseInfo.m
@@ -212,8 +212,7 @@ static NSString *kQNErrorDomain = @"qiniu.com";
 
 - (BOOL)couldHostRetry{
     if ([self couldRegionRetry] == NO
-        || _statusCode == 502 || _statusCode == 503 || _statusCode == 504 || _statusCode == 571
-        || _statusCode == 599) {
+        || _statusCode == 502 || _statusCode == 503 || _statusCode == 571 || _statusCode == 599) {
         return NO;
     } else {
         return YES;
