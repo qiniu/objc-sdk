3e2d8575eda9129c89f4df76c9d6e53ca0b8488e
YangSen-qn <yang_sen_ysys@163.com>
2020-12-23 15:57:31 +0800

    concurrent resume test add default progress handler

--- QiniuSDKTests/QNConcurrentResumeUploadTest.m
+++ QiniuSDKTests/QNConcurrentResumeUploadTest.m
@@ -21,6 +21,8 @@
 
 - (void)setUp {
     kQNGlobalConfiguration.isDnsOpen = YES;
+    [super setUp];
+    
 }
 
 - (void)tearDown {
@@ -33,7 +35,6 @@
         builder.useHttps = YES;
     }];
     NSArray *sizeArray = @[@5000, @8000, @10000, @20000];
-    sizeArray = @[@5000];
     for (NSNumber *size in sizeArray) {
         NSString *key = [NSString stringWithFormat:@"concurrent_resume_switch_region_v1_%@k", size];
         QNTempFile *tempFile = [QNTempFile createTempFileWithSize:[size intValue] * 1024 identifier:key];
